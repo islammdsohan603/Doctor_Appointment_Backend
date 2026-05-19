d52c7377d9e01b3536c25f89611c135457f390c0
islammdsohan603 <islammdsohan603@gmail.com>
2026-05-19 07:19:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+
+app.use(express.json());
+
+const port = process.env.PORT;
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    const db = client.db("doctors");
+    const doctorData = db.collection("doctorslist");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
