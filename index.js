8d3e77d17ed0f26251cafe20533dc3b85049eb4d
islammdsohan603 <islammdsohan603@gmail.com>
2026-05-20 11:14:31 +0600

    delet bookings data

--- index.js
+++ index.js
@@ -80,6 +80,13 @@ const run = async () => {
       res.send(result);
     });
 
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await booking.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
